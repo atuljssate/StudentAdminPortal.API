e718f593b782ace2f4a45ead0508193b988ce279
Kumar, Atul <atuljssate@gmail.com>
2022-10-07 17:14:28 +0530

    student with id

--- StudentAdminPortal.API/StudentAdminPortal.API/Repositories/SqlStudentRepository.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Repositories/SqlStudentRepository.cs
@@ -14,7 +14,15 @@ namespace StudentAdminPortal.API.Repositories
 
         public async Task<List<Student>> GetStudentsAsync()
         {
-            return await context.Student.Include(nameof(Gender)).Include(nameof(Address)).ToListAsync();
+            return await context.Student
+                .Include(nameof(Gender)).Include(nameof(Address)).ToListAsync();
+        }
+
+        public async Task<Student> GetStudentAsync(Guid studentId)
+        {
+            return await context.Student
+                .Include(nameof(Gender)).Include(nameof(Address))
+                .FirstOrDefaultAsync(x => x.Id==studentId);
         }
     }
 }
